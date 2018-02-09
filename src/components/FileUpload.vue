0e88757f5dee127826512cd703a007f5838429f9
isaac lessard <isaaclessard@isaacs-MacBook-Pro.local>
2018-02-08 18:30:41 -0700

    :wrench: initial fileupload view

---
+++ src/components/FileUpload.vue
@@ -0,0 +1,78 @@
+<template lang="html">
+    <html>
+    <head>
+    </head>
+    <body>
+      <div id='fileUpload'>
+        <form class="formBody">
+          <section class="fileSubmission">
+            <label>Files to Submit</label>
+            <input>
+            <button>Browse</button>
+          </section>
+          <section class="pipelineSelection">
+            <input type="radio" value="WGS" v-model="type"> Whole Genome Sequence Pipeline
+            <input type="radio" value="TSP" v-model="type"> Targeted Sequence Pipeline
+          </section>
+          <h3>Selected: {{ type }}</h3>
+          <div class="queueInfo">
+            <label>Current WGS Queue Size: </label>
+            <input v-model="wsgQueue" disabled>
+            <label>Current TGS Queue Size: </label>
+            <input v-model="tgsQueue" disabled>
+          </div>
+          <div class="uploadBtn">
+          <button type="submit">Upload</button>
+        </div>
+        </form>
+      </div>
+    </body>
+
+    </html>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      type: 'WGS',
+      wsgQueue: '84',
+      tgsQueue: '35',
+    };
+  },
+};
+</script>
+
+<style scoped>
+body {
+  min-width: 750px;
+}
+.formBody {
+  margin: auto;
+  width: 80%;
+  border: 3px solid gray;
+  padding: 40px;
+  text-align: center;
+}
+
+.fileSubmission button {
+  margin: 20px;
+}
+
+.fileSubmission input {
+  margin: 20px;
+}
+
+.pipelineSelection input {
+  margin: 20px;
+}
+
+.queueInfo input {
+  width: 50px;
+}
+
+.uploadBtn {
+  margin-top: 25px;
+  text-align: center;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Dashboard from '@/components/Dashboard';
+import FileUpload from '@/components/FileUpload';
 
 
 Vue.use(Router);
 export default new Router({
   routes: [
     { path: '/dashboard', component: Dashboard },
+    { path: '/fileupload', component: FileUpload },
   ],
 });
