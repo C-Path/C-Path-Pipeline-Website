993856c5c4bed14e383ec19b0b8dc1ac4a460b1b
Merrill Lines <linesmerrill@gmail.com>
2018-02-10 10:37:27 -0700

    removed styling from app.vue to prevent it overwriting styles for individual components

--- src/App.vue
+++ src/App.vue
@@ -11,9 +11,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
+
 </style>
