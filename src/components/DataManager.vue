aad969d70c5f4002301cceb6ae05e34f70a5ffac
isaac lessard <isaaclessard@isaacs-MacBook-Pro.local>
2018-02-10 12:00:29 -0700

    :clipboard: initial data manager page

---
+++ src/components/DataManager.vue
@@ -0,0 +1,76 @@
+<template lang="html">
+    <html>
+    <head>
+    </head>
+    <body>
+      <div class="tableBody">
+      <table>
+        <tr>
+          <th>Select</th>
+          <th class="dataFile">Data File</th>
+        </tr>
+        <tr v-for="file in files">
+          <td><input type="checkbox" v-model=file.selected></td>
+          <td>{{file.name}}</td>
+        </tr>
+      </table>
+      <div class="button">
+        <button>Transfer</button>
+    </div>
+    </form>
+  </div>
+    </body>
+
+    </html>
+</template>
+
+<script>
+export default {
+  methods: {
+
+  },
+  data() {
+    return {
+      files: [{selected: true, name: "SRR_102237_1.fastq"},{selected: false, name: "SRR_3324_1.fastq.fastq"},{selected: false, name: "SRR_3475_1.fastq"},{selected: false, name: "SRR_3475_2.fastq"}],
+      wsgQueue: '84',
+      tgsQueue: '35',
+    };
+  },
+};
+</script>
+
+<style scoped>
+body {
+  min-width: 750px;
+}
+
+table, th, td {
+  border: 1px solid black;
+  border-collapse: collapse;
+}
+
+table {
+  margin: auto;
+  margin-bottom: 50px;
+}
+
+th {
+  background-color: #D3D3D3;
+  padding: 20px;
+}
+
+.tableBody {
+  margin: auto;
+  width: 80%;
+  border: 3px solid gray;
+  padding: 40px;
+}
+
+.dataFile {
+  width: 80%;
+}
+
+.button {
+  text-align: center;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router';
 import Dashboard from '@/components/Dashboard';
 import FileUpload from '@/components/FileUpload';
 import JobStatus from '@/components/JobStatus';
+import DataManager from '@/components/DataManager';
 
 
 Vue.use(Router);
@@ -11,5 +12,6 @@ export default new Router({
     { path: '/dashboard', name: 'Dashboard', component: Dashboard },
     { path: '/fileupload', name: 'FileUpload', component: FileUpload },
     { path: '/jobstatus', name: 'JobStatus', component: JobStatus },
+    { path: '/datamanager', name: 'DataManager', component: DataManager },
   ],
 });
