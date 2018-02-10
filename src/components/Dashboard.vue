15eb0c3132c6ff7a6d528de57c3e50c436fe79c4
Merrill Lines <linesmerrill@gmail.com>
2018-02-10 10:46:54 -0700

    add button to Add New Files

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -8,19 +8,21 @@
             <!-- table -->
         </div>
         <div>
-            <button @click="show">Create New Project</button>
-           <modal name="NewProjectModal" height="auto" :scrollable="true">
+            <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" @click="show">Create New Project</button>
+            <modal name="NewProjectModal" height="auto" :scrollable="true">
                <h3 class="text-align-center">Create New Project</h3>
                <div class="text-align-center">
-                   <label for="project-name">Project name:</label>
+                <label for="project-name">Project name:</label>
                    <input id="project-name" name="projectName" placeholder="Project name" required>
                </div>
                <br>
                <div class="text-align-center">
-               <button  @click="hide">Create</button>
-
+                    <button  @click="hide">Create</button>
                 </div>
            </modal>
+           <form action="/fileupload">
+            <input class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" type="submit" value="Add New File" />
+            </form>
         </div>
     </body>
 
