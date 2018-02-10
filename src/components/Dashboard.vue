f1a6306890e9856cb6616c5c0808b8167e761ec6
Merrill Lines <linesmerrill@gmail.com>
2018-02-10 11:43:57 -0700

    add table to user projects page

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -2,13 +2,30 @@
     <html>
     <head>
     </head>
-    <body>
+    <body class="margin-left-3">
         <h3>User Projects</h3>
-        <div>
-            <!-- table -->
+        <div class="margin-botton-2">
+            <table class="mdl-data-table mdl-js-data-table mdl-data-table--selectable">
+                <thead>
+                    <tr>
+                    <th class="mdl-data-table__cell--non-numeric">Project Name</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <td class="mdl-data-table__cell--non-numeric">Research Project 2</td>
+                    </tr>
+                    <tr>
+                    <td class="mdl-data-table__cell--non-numeric">Surveillance Project 1</td>
+                    </tr>
+                    <tr>
+                    <td class="mdl-data-table__cell--non-numeric">EU Study</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
         <div>
-            <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" @click="show">Create New Project</button>
+            <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect margin-right-2" @click="show">Create New Project</button>
             <modal name="NewProjectModal" height="auto" :scrollable="true">
                <h3 class="text-align-center">Create New Project</h3>
               <form action="#">
@@ -22,9 +39,9 @@
                    <button @click="hide" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">Add Project</button>
                 </div>
            </modal>
-           <form action="/#/fileupload">
-            <input class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" type="submit" value="Add New File" />
-            </form>
+           <a href="/#/fileupload">
+                <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">Add New File</button>
+            </a>
         </div>
     </body>
 
@@ -57,5 +74,13 @@ export default {
 
 .margin-botton-2 {
     margin-bottom: 2em;
+}
+
+.margin-right-2 {
+    margin-right: 2em;
+}
+
+.margin-left-3 {
+    margin-left: 3em;
 }
 </style>
