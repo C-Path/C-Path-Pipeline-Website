d27a0e3823edb9882d6f9d9e0e2dff1e05684724
isaac lessard <isaaclessard@isaacs-MacBook-Pro.local>
2018-02-16 16:14:13 -0700

    :wrench: initial auth

--- src/auth.js
+++ src/auth.js
@@ -1,23 +1,51 @@
 /* globals localStorage */
+import axios from "axios";
 
 export default {
-  login (email, pass, cb) {
+  // login (email, pass, cb) {
+  //   cb = arguments[arguments.length - 1]
+  //   if (localStorage.token) {
+  //     if (cb) cb(true)
+  //     this.onChange(true)
+  //     return
+  //   }
+  //   pretendRequest(email, pass, (res) => {
+  //     if (res.authenticated) {
+  //       localStorage.token = res.token
+  //       localStorage.setItem('user', JSON.stringify(res))
+  //       if (cb) cb(true)
+  //       this.onChange(true)
+  //     } else {
+  //       if (cb) cb(false)
+  //       this.onChange(false)
+  //     }
+  //   })
+  // },
+  login (username, pass, cb) {
     cb = arguments[arguments.length - 1]
     if (localStorage.token) {
       if (cb) cb(true)
       this.onChange(true)
       return
     }
-    pretendRequest(email, pass, (res) => {
+    var userData = {
+      usnermae: username,
+      password: pass
+    }
+    axios.post("http://localhost:3000/authenticate", userData).then(function(res) {
       if (res.authenticated) {
+        console.log('true')
         localStorage.token = res.token
         localStorage.setItem('user', JSON.stringify(res))
         if (cb) cb(true)
         this.onChange(true)
       } else {
         if (cb) cb(false)
+        console.log('false')
         this.onChange(false)
       }
+    }).catch(function(err) {
+      console.log(err)
     })
   },
 
