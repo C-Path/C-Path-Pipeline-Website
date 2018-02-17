e7b4be7a27b7f51254d10ccd69ccd16fba9994fd
Merrill Lines <linesmerrill@gmail.com>
2018-02-17 08:19:53 -0700

    only show projects btn if logged in

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
       <router-link v-if="loggedIn" to="/logout">
         <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect log-out">Log Out</button>
       </router-link>
-      <a href="/dashboard">
+      <a href="/dashboard" v-if="loggedIn">
         <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect log-out">Projects</button> 
         </a>                   
     </div>
