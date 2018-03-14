e5df06e7b6d57a0e0b367a2a618e4832e2fe7041
Merrill Lines <linesmerrill@gmail.com>
2018-03-13 18:36:46 -0700

    pass in username and password from oauth to /authenticate route

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,13 +1,40 @@
-<template>
-  <div class="signInArea col-md-offset-4">
-    <h2 class="padding-title">Welcome, Sign in Here</h2>
-  <g-signin-button
-    :params="googleSignInParams"
-    @success="onSignInSuccess"
-    @error="onSignInError">
-    Sign in with Google
-  </g-signin-button>
-  </div>
+<template lang="html">
+    <html>
+    <head>
+    </head>
+    <body>
+      <section class="text-align-center error">
+          <h4 v-if="$route.query.redirect">
+            Please log in to access this website.
+          </h4>
+      </section>
+        <section class="login">
+          <div class="align-center text-align-center">
+          	<main class="mdl-layout__content mdl-shadow--6dp">
+          		<div class="mdl-card mdl-shadow--6dp">
+          			<div class="mdl-card__title mdl-color--primary mdl-color-text--white">
+          				<h2 class="mdl-card__title-text">Welcome.</h2>
+          			</div>
+          	  	<div class="mdl-card__supporting-text">
+                  <g-signin-button
+                  :params="googleSignInParams"
+                  @success="onSignInSuccess"
+                  @error="onSignInError">
+                  Sign in with Google
+                </g-signin-button>
+          			</div>
+          		</div>
+          	</main>
+          </div>
+        </section>
+        <section class="text-align-center account-services">
+          <p>If you would like an account, or if you forgot your credentials,<br> please click the link below to send us an email</p>
+          <a href="mailto:isaac.c.lessard@gmail.com?subject=ReSeqTB Account Services">
+            <button class="margin-bottom-2 mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">Contact Us</button>
+          </a>
+        </section>
+      </body>
+    </html>
 </template>
 
 <script>
@@ -18,14 +45,27 @@ export default {
     return {
       googleSignInParams: {
         client_id: process.env.CLIENT_ID + ".apps.googleusercontent.com"
-      }
+      },
+      username: '',
+        pass: '',
+        error: false
     };
   },
   methods: {
     onSignInSuccess(googleUser) {
-
-     console.log("successfully authenticated")
-      
+      this.username = googleUser.w3.U3
+      this.pass = googleUser.w3.Eea
+      auth.login(this.username, this.pass, (loggedIn) => {
+          if (!loggedIn) {
+            this.error = true
+          } else {
+            if (auth.isManager()) {
+              this.$router.replace(this.$route.query.redirect || '/datamanager')
+            } else {
+              this.$router.replace(this.$route.query.redirect || '/dashboard')
+            }
+          }
+        })
     },
     onSignInError(error) {
       console.log("error: ", error);
@@ -44,20 +84,6 @@ export default {
   color: #fff;
   box-shadow: 0 3px 0 #0f69ff;
   width: 12em;
-  height: 2em;
   font-size: 18px;
-}
-
-.padding-title {
-  padding-top: 1em;
-  padding-bottom: 1em;
-}
-
-.signInArea {
-  background-color: gainsboro;
-  width: 35%;
-  margin-top: 2em;
-  text-align: center;
-  height: 14em;
 }
 </style>
