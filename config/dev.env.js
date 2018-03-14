e9886ba44e2a5d63d536a9d64ca4e8f7491d8f02
Merrill Lines <linesmerrill@gmail.com>
2018-03-13 17:38:30 -0700

    add oauth for google for login

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  SERVER_URL: '"http://localhost:3000"'
+  SERVER_URL: '"http://localhost:3000"',
+  CLIENT_ID: '"444399542573-802e5tggcegcc20ei5idq9h9q6sqaurg"'
 })
