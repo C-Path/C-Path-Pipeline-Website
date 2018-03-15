e1a482f636c29d4eddfbef4361fa39ce850d2708
Merrill Lines <linesmerrill@gmail.com>
2018-03-14 19:41:28 -0700

    add in modal to request new account

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -30,10 +30,33 @@
         </section>
         <section class="text-align-center account-services">
           <p>If you would like an account, or if you forgot your credentials,<br> please click the link below to send us an email</p>
-          <a href="mailto:isaac.c.lessard@gmail.com?subject=ReSeqTB Account Services">
-            <button class="margin-bottom-2 mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">Contact Us</button>
-          </a>
+          
+            <button @click="show()" class="margin-bottom-2 mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">Contact Us</button>
+          
+          <modal class="" name="ContactUsModal" height="auto" :scrollable="true">
+             <div class="text-align-center">
+               <h3 class="text-underline">Request New Account</h3>
+             </div>
+              <div class="text-align-center">
+                <p> 1. Sign in with the account you would like to be created.</p>
+                <g-signin-button
+                  :params="googleSignInParams"
+                  @success="createSuccess"
+                  @error="createError">
+                  Sign in
+                </g-signin-button>
+                <div v-if="authWithGoogle">
+                  <p>2. Ok Great! Let's send a request to have the account <b>{{googleUser.w3.U3}}</b> created.</p>
+                  <button @click="sendRequest" class="margin-bottom-2 mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">Send Request</button>
+                </div>
+                <div v-if="requestSent">
+                  <p >3. Easy as that! Once your request is reviewed, you will be notified at <b>{{googleUser.w3.U3}}</b>.</p>
+                  <button @click="close" class="margin-bottom-2 mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">Close</button>
+                </div>
+              </div>
+              </modal>
         </section>
+        <a :href="myValue" id="mymailto" style="display:none"></a>
       </body>
     </html>
 </template>
@@ -49,7 +72,11 @@ export default {
       },
       username: "",
       pass: "",
-      error: false
+      error: false,
+      authWithGoogle: false,
+      googleUser: "",
+      myValue: "mailto:",
+      requestSent: false
     };
   },
   methods: {
@@ -70,6 +97,32 @@ export default {
     },
     onSignInError(error) {
       console.log("error: ", error);
+    },
+    show() {
+      this.$modal.show("ContactUsModal");
+    },
+    close() {
+      this.authWithGoogle = false;
+      this.googleUser = "";
+      this.requestSent = false;
+      this.$modal.hide("ContactUsModal");
+    },
+    createSuccess(googleUser) {
+      this.googleUser = googleUser;
+      this.authWithGoogle = true;
+      var body =
+        "body=Username: " +
+        this.googleUser.w3.U3 +
+        "%0d%0aID: " +
+        this.googleUser.w3.Eea;
+      this.myValue = "mailto:ken@cpath.com?subject=New Account Request&" + body;
+    },
+    createError(error) {
+      console.log("error: ", error);
+    },
+    sendRequest() {
+      document.getElementById("mymailto").click();
+      this.requestSent = true;
     }
   }
 };
@@ -86,6 +139,7 @@ export default {
   box-shadow: 0 3px 0 #0f69ff;
   width: 12em;
   font-size: 18px;
+  margin-bottom: 2em;
 }
 
 .error {
