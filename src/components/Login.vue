8902a17976b7210e2cf07ff754cfb16cfd1d5074
Merrill Lines <linesmerrill@gmail.com>
2018-03-14 19:47:45 -0700

    cleanup code styling

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,64 +1,62 @@
 <template lang="html">
-    <html>
+  <html>
     <head>
     </head>
     <body>
       <section class="text-align-center error">
-          <h4 v-if="$route.query.redirect">
-            Please log in to access this website.
-          </h4>
+        <h4 v-if="$route.query.redirect">
+          Please log in to access this website.
+        </h4>
       </section>
-        <section class="login">
-          <div class="align-center text-align-center">
-          	<main class="mdl-layout__content mdl-shadow--6dp">
-          		<div class="mdl-card mdl-shadow--6dp">
-          			<div class="mdl-card__title mdl-color--primary mdl-color-text--white">
-          				<h2 class="mdl-card__title-text">Welcome.</h2>
-          			</div>
-          	  	<div class="mdl-card__supporting-text">
-                  <g-signin-button
-                  :params="googleSignInParams"
-                  @success="onSignInSuccess"
-                  @error="onSignInError">
-                  Sign in with Google
-                </g-signin-button>
-          			</div>
+      <section class="login">
+        <div class="align-center text-align-center">
+          <main class="mdl-layout__content mdl-shadow--6dp">
+          	<div class="mdl-card mdl-shadow--6dp">
+          		<div class="mdl-card__title mdl-color--primary mdl-color-text--white">
+          			<h2 class="mdl-card__title-text">Welcome.</h2>
           		</div>
-          	</main>
-            <p v-if="error" class="error">Incorrect username or password</p>
-          </div>
-        </section>
-        <section class="text-align-center account-services">
-          <p>If you would like an account, or if you forgot your credentials,<br> please click the link below to send us an email</p>
-          
-            <button @click="show()" class="margin-bottom-2 mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">Contact Us</button>
-          
-          <modal class="" name="ContactUsModal" height="auto" :scrollable="true">
-             <div class="text-align-center">
-               <h3 class="text-underline">Request New Account</h3>
-             </div>
-              <div class="text-align-center">
-                <p> 1. Sign in with the account you would like to be created.</p>
+          	  <div class="mdl-card__supporting-text">
                 <g-signin-button
-                  :params="googleSignInParams"
-                  @success="createSuccess"
-                  @error="createError">
-                  Sign in
-                </g-signin-button>
-                <div v-if="authWithGoogle">
-                  <p>2. Ok Great! Let's send a request to have the account <b>{{googleUser.w3.U3}}</b> created.</p>
-                  <button @click="sendRequest" class="margin-bottom-2 mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">Send Request</button>
-                </div>
-                <div v-if="requestSent">
-                  <p >3. Easy as that! Once your request is reviewed, you will be notified at <b>{{googleUser.w3.U3}}</b>.</p>
-                  <button @click="close" class="margin-bottom-2 mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">Close</button>
-                </div>
-              </div>
-              </modal>
-        </section>
-        <a :href="myValue" id="mymailto" style="display:none"></a>
-      </body>
-    </html>
+                :params="googleSignInParams"
+                @success="onSignInSuccess"
+                @error="onSignInError">
+                Sign in with Google
+              </g-signin-button>
+          	</div>
+          </div>
+        </main>
+        <p v-if="error" class="error">Incorrect username or password</p>
+      </div>
+      </section>
+      <section class="text-align-center account-services">
+        <p>If you would like an account, or if you forgot your credentials,<br> please click the link below to send us an email</p>
+        <button @click="show()" class="margin-bottom-2 mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">Contact Us</button>
+        <modal class="" name="ContactUsModal" height="auto" :scrollable="true">
+          <div class="text-align-center">
+            <h3 class="text-underline">Request New Account</h3>
+          </div>
+          <div class="text-align-center">
+            <p> 1. Sign in with the account you would like to be created.</p>
+            <g-signin-button
+              :params="googleSignInParams"
+              @success="createSuccess"
+              @error="createError">
+              Sign in
+            </g-signin-button>
+            <div v-if="authWithGoogle">
+              <p>2. Ok Great! Let's send a request to have the account <b>{{googleUser.w3.U3}}</b> created.</p>
+              <button @click="sendRequest" class="margin-bottom-2 mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">Send Request</button>
+            </div>
+            <div v-if="requestSent">
+              <p >3. Easy as that! Once your request is reviewed, you will be notified at <b>{{googleUser.w3.U3}}</b>.</p>
+              <button @click="close" class="margin-bottom-2 mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">Close</button>
+            </div>
+          </div>
+        </modal>
+      </section>
+      <a :href="myValue" id="mymailto" style="display:none"></a>
+    </body>
+  </html>
 </template>
 
 <script>
@@ -130,7 +128,6 @@ export default {
 
 <style>
 .g-signin-button {
-  /* This is where you control how the button looks. Be creative! */
   display: inline-block;
   padding: 4px 8px;
   border-radius: 3px;
