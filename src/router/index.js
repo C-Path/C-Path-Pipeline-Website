2aa8a61b11277c149726164621a5cff148098f5c
isaac lessard <isaaclessard@isaacs-MacBook-Pro.local>
2018-03-21 17:05:55 -0700

    removed TGen files

--- src/router/index.js
+++ src/router/index.js
@@ -11,12 +11,6 @@ const router = new Router({
     {path: '/login', name: 'Login', component: () => import('@/components/Login'), beforeEnter: checkToken},
     {path: '/dashboard', name: 'Dashboard', component: () => import('@/components/Dashboard'), beforeEnter: requireAuth},
     {path: '/datamanager', name: 'DataManager', component: () => import('@/components/DataManager'), beforeEnter: requireAdmin},
-    {path: '/upload', component: () => import('@/pages/tnorth/asap/fileupload/SequenceFileUpload')},
-    {path: '/projects/default', component: () => import('@/pages/tnorth/asap/FileIndex')},
-    {path: '/projects/default/reports/tb', component: () => import('@/pages/tnorth/asap/tb/Summary.vue'), beforeEnter: requireAuth},
-    {path: '/projects/default/reports/tb/samples/:id', component: () => import('@/pages/tnorth/asap/tb/Sample.vue'), props: true, beforeEnter: requireAuth},
-    {path: 'projects/default/reports/tb/detail', component: () => import('@/pages/tnorth/asap/tb/SummaryDetail.vue'), beforeEnter: requireAuth},
-    {path: '/projects/default/reports/tb/samples/:id/detail', component: () => import('@/pages/tnorth/asap/tb/SampleDetail.vue'), props: true, beforeEnter: requireAuth},
     {path: '*', beforeEnter: redirectUser },
     {path: '/logout',
       beforeEnter (to, from, next) {
